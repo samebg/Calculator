77d3baff88e3fc6b2a05e751eeb9a9cbcfa2fa47
samebg <samebengho@gmail.com>
2025-03-19 13:52:06 -0700

    UPDATED SDK pt2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
 
-        int sum = calculator.add(5, 3);
-        System.out.println("5 + 3 = " + sum);
-
+        int sum = calculator.add(10, 5);
+        System.out.println("10 + 5 = " + sum);
     }
 }
 
